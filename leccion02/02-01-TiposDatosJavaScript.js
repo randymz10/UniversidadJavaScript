d24a3872e3e48f32b95358725fc11eb68263c46a
randymz10 <randymz10@gmail.com>
2022-04-09 21:22:26 -0300

    Tipo de dato en Javascript

--- leccion02/02-01-TiposDatosJavaScript.js
+++ leccion02/02-01-TiposDatosJavaScript.js
@@ -2,5 +2,20 @@
 
 /*Tipo de dato String */
 
-var nombre = "Carlos";
-console.log(nombre);
+var string = "Carlos";
+console.log(string);
+
+
+// Tipo de dato numérico
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato object
+
+var objeto = {
+    nombre: "Randy",
+    apellido: "Muñoz",
+    edad: 24
+}
+
+console.log(objeto);
