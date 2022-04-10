ddd8590f7dfa64128703ea10f7828f16918fbdc4
randymz10 <randymz10@gmail.com>
2022-04-09 21:37:06 -0300

    Otros tipos de datos

--- leccion02/02-01-TiposDatosJavaScript.js
+++ leccion02/02-01-TiposDatosJavaScript.js
@@ -20,6 +20,41 @@ var objeto = {
 
 console.log(objeto);
 
+//Tipo de dato boolean (true, false)
+var bandera = true; 
+console.log(bandera);
+
+//Tipo de dato function
+
+function miFuncion(){}
+console.log(miFuncion);
+
+// Tipo de dato Symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+
+//Tipo clase es una function
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(typeof Persona);
+
+//Tipo undefined
+var x;
+console.log(x);
+
+x = undefined;
+console.log(typeof x);
+
+//Tipo null = ausencia de valor
+
+var y = null;
+console.log(typeof y)
+
 //TypeOf en JavaScript
 
 console.log(typeof string);
