af23afe352639d7238bcb64aa00b5742a84dd468
randymz10 <randymz10@gmail.com>
2022-04-12 16:21:50 -0300

    Adicionado operadores aritméticos

---
+++ leccion03/03-10-OperadoresJavaScript.js
@@ -0,0 +1,25 @@
+//Operador (+)
+let a = 3, b = 2;
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+//Operador (-)
+z = a - b;
+console.log("Resultado de la resta: " + z);
+
+//Operador (*)
+z = a * b;
+console.log("Resultado de la multiplicación: " + z);
+
+//Operador (/)
+z = a / b;
+console.log("Resultado de la división: " + z);
+
+//Operador (%)
+z = a % b; // residuo de la división
+console.log("Resultado del modulo (residuo): " + z);
+
+//Operador exponente (**)
+z = a ** b;
+console.log("Resultado del exponente: " + z);
+
