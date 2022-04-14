899a4a84287c27c74752994d65f776da4b8ca902
randymz10 <randymz10@gmail.com>
2022-04-14 15:51:55 -0300

    Adicionado ejercicios de ejemplo

---
+++ leccion03/03-02-Ejercicios.js
@@ -0,0 +1,19 @@
+// Ejercicio número par
+
+let a = 9;
+
+if (a % 2 == 0){
+    console.log("Es un número par")
+} else {
+    console.log("Es un número impar")
+}
+
+// Ejercicio mayor de edad
+
+let edad = 20, adulto = 18;
+
+if( edad >= adulto ){
+    console.log("Es un adulto");
+}else{
+    console.log("Es menor de edad");
+}
