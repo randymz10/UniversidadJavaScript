b77794c2f6492d25bbdd99c3cc0d9531b841966e
randymz10 <randymz10@gmail.com>
2022-04-23 00:28:31 -0300

    Add example of functions

--- leccion07/07-01-FuncionesJS-UJS.js
+++ leccion07/07-01-FuncionesJS-UJS.js
@@ -51,3 +51,15 @@ console.log(typeof miFuncion);
 var miFuncionTexto = miFuncion.toString();
 console.log(miFuncionTexto);
 
+//Ejemplo funciones
+
+
+resultado = sumarTodo(5, 4, 13, 10, 9);
+console.log(resultado);
+function sumarTodo(){
+    let suma = 0;
+    for(let i = 0; i < arguments.length; i++){
+        suma += arguments[i]; // suma = suma + arguments[i]
+    }
+    return suma
+}
